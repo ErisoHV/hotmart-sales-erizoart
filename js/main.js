6c2cc3eae66884f6b0233bc59287eb45f1660d19
ErisoHV <erika.programmer@gmail.com>
2025-06-06 20:18:33 -0500

    Main template

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ function fillSection(htmlPage, section){
     fetch(htmlPage)
         .then(res => res.text())
         .then(data => {
-            document.getElementById(section).innerHTML = data;
-
-            document.querySelectorAll('.accordion-button').forEach(button => {
-                button.addEventListener('click', () => {
-                    const item = button.parentElement;
-                    item.classList.toggle('active');
+            const container = document.getElementById(section);
+            container.innerHTML = data;
+            if (container.querySelectorAll('.accordion-button') !== null) {
+                container.querySelectorAll('.accordion-button').forEach(button => {
+                    button.addEventListener('click', () => {
+                        const item = button.parentElement;
+                        item.classList.toggle('active');
+                    });
                 });
-            });
+            }
 
         });
 }
